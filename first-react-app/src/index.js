ad700cf3c45c48671e1c606a5b5af345c648df7f
celinefu <celinefu@tencent.com>
2017-10-12 19:13:54 +0800

    add chapter2

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import ClickCounter from './ClickCounter';
-import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
-ReactDOM.render(<ClickCounter />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <ClickCounter />,
+  document.getElementById('root')
+);
